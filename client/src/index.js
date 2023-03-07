3a4c3d618c9f2323103eccbd78a1461aa90ffce9
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-03-07 15:51:43 +0100

    assignment part 2 compelted

--- client/src/index.js
+++ client/src/index.js
@@ -1,39 +1,54 @@
 function registerEventListeners (){
     var orderedItems = document.getElementsByClassName("orderbutton")
-    console.log(orderedItems)
-
 
     for (var i = 0; i < orderedItems.length ; i++) {
         var button = orderedItems[i]
         var nameOfAttraction
         var numAdults
         var numKids
+        var basket 
+        var dictBasket = {}
+        var NumberOfOrders = 0
         button.addEventListener('click', function(event) {
             var buttonClicked = event.target
             nameOfAttraction = buttonClicked.parentElement.parentElement.firstElementChild.innerText
-            // console.log(nameOfAttraction)
-
 
             var classNumAdults = buttonClicked.parentElement.getElementsByClassName("numberofadults")
             if (classNumAdults.length >0 ) {
                 numAdults = classNumAdults[0].value
-                // console.log(numAdults)
             }
 
             var classNumKids = buttonClicked.parentElement.getElementsByClassName("numberofkids")
             if (classNumKids.length >0 ) {
                 numKids = classNumKids[0].value
-                // console.log(numKids)
             }
-            saveOrderInShoppingBasket(numAdults, numKids, nameOfAttraction)
+
+
+            basket = {
+                "Name of attraction:" : nameOfAttraction,
+                "Number of adults:" : numAdults,
+                "Number of kids:" : numKids
+            }
+
+            NumberOfOrders =NumberOfOrders +1
+            dictBasket[(NumberOfOrders).toString()] = basket
+            saveOrderInShoppingBasket(dictBasket)
+
+            document.getElementsByClassName("badge")[0].innerText =  (NumberOfOrders).toString()
+            console.log(document.getElementsByClassName("badge")[0].innerText)
+
         })
         
     }
 
 }
 
-function saveOrderInShoppingBasket(numAdults, numKids, nameOfAttraction) {
-    console.log(numAdults, numKids, nameOfAttraction)
-}
+function saveOrderInShoppingBasket(dictBasket) {
+    for (var key in dictBasket) {
+        localStorage.setItem(key, JSON.stringify(dictBasket[key]))
+        localStorage.getItem(key)
+      }
+  }
 
 registerEventListeners ()
+
