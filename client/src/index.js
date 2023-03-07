582f96479f54faac7567f4ad6a5b07fa3dfed3f0
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-03-07 12:24:37 +0100

    get name of the attraction, numbers of adults and kids

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,39 @@
-var orderedItems = document.getElementsByClassName("orderbutton")
-console.log(orderedItems)
+function registerEventListeners (){
+    var orderedItems = document.getElementsByClassName("orderbutton")
+    console.log(orderedItems)
 
-for (var i = 0; i < orderedItems.length ; i++) {
-    var button = orderedItems[i]
-    button.addEventListener('click', function(event){
-        var buttonClicked = event.target
-        console.log(buttonClicked.parentElement.parentElement.querySelectorAll("parkname"))
-        // console.log(buttonClicked.classList.contains())
-        // console.log(buttonClicked.nextElementSibling)
-        // console.log(buttonClicked.previousElementSibling)
-    })
+
+    for (var i = 0; i < orderedItems.length ; i++) {
+        var button = orderedItems[i]
+        var nameOfAttraction
+        var numAdults
+        var numKids
+        button.addEventListener('click', function(event) {
+            var buttonClicked = event.target
+            nameOfAttraction = buttonClicked.parentElement.parentElement.firstElementChild.innerText
+            // console.log(nameOfAttraction)
+
+
+            var classNumAdults = buttonClicked.parentElement.getElementsByClassName("numberofadults")
+            if (classNumAdults.length >0 ) {
+                numAdults = classNumAdults[0].value
+                // console.log(numAdults)
+            }
+
+            var classNumKids = buttonClicked.parentElement.getElementsByClassName("numberofkids")
+            if (classNumKids.length >0 ) {
+                numKids = classNumKids[0].value
+                // console.log(numKids)
+            }
+            saveOrderInShoppingBasket(numAdults, numKids, nameOfAttraction)
+        })
+        
+    }
+
 }
+
+function saveOrderInShoppingBasket(numAdults, numKids, nameOfAttraction) {
+    console.log(numAdults, numKids, nameOfAttraction)
+}
+
+registerEventListeners ()
