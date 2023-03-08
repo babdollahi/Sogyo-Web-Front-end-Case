063b8c8a3f5fee3717b5cda6c51106d0c1d9597e
Banafsheh Abdollahi <babdollahi@sogyo.nl>
2023-03-08 12:12:18 +0100

    adds node for each order

--- client/src/shoppingbasket.js
+++ client/src/shoppingbasket.js
@@ -1,7 +1,21 @@
 const orders = JSON.parse(localStorage.getItem('orders'))
 
-for (i in orders) {
-    console.log(orders[i]["Name of attraction"])
-    console.log(parseInt(orders[i]["Number of adults"]))
-    console.log(parseInt(orders[i]["Number of kids"]))
+function addOrder(order) {
+    var temp = document.getElementsByTagName("template")[0];
+    var clon = temp.content.cloneNode(true);
+    var divs = []
+    var divs = clon.querySelectorAll("div")
+    divs[0].innerText = "Park Name: " + order["Name of attraction"]
+    divs[1].innerText = "Number of Adults: " + order["Number of adults"]
+    divs[2].innerText = "Number of Kids: " + order["Number of kids"]
+    document.body.appendChild(clon);
+  }
+
+function showContent() {
+  for (i in orders) {
+    addOrder(orders[i])
+    }
 }
+
+showContent()
+
